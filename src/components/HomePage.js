87947d95ca563f649021abddf51488213dfd5140
salik03 <uddinsalik03@gmail.com>
2023-11-05 22:22:07 +0530

    first commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+
+function HomePage() {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('/blogdata.txt')
+      .then((response) => response.text())
+      .then((data) => {
+        const lines = data.split('\n');
+        const posts = lines.map((line, index) => {
+          const [title, content, imageUrl] = line.split('|');
+          return { id: index, title, content, imageUrl };
+        });
+        setBlogPosts(posts);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <Typography variant="h3" sx={{ my: 3 }}>
+        My Blogspot
+      </Typography>
+      <Grid container spacing={2}>
+        {blogPosts.map((post) => (
+          <Grid item key={post.id} xs={12} sm={6} md={4}>
+            <Card sx={{ height: '100%' }}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={post.imageUrl} 
+                alt={post.title} 
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {post.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {post.content}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
+
+export default HomePage;
